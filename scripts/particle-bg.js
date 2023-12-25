006c8f51c0823a65b025841c45f684e893e57771
as9219 <amansahu9000@gmail.com>
2023-12-25 01:48:28 -0500

    dynamic background

---
+++ scripts/particle-bg.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      connectParticles: true,
+      maxParticles: 150,
+      color: '#77a9f8',
+      sizeVariations: 3
+    });
+  };
