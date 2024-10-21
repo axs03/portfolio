60b8a00c94434effbb9f9f34085280eb20304b25
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-10-21 18:46:27 -0400

    moving to django

--- scripts/particle-bg.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    Particles.init({
-      selector: '.background',
-      connectParticles: true,
-      maxParticles: 100,
-      color: '#ffffff',
-      sizeVariations: 5
-    });
-};
