5d2acbc1597186c1e03be349171b57eb988c24a1
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-10-21 19:43:38 -0400

    basic landing works
    landing page works
    now on django framework

---
+++ mainfiles/static/js/particle-bg.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      connectParticles: true,
+      maxParticles: 100,
+      color: '#ffffff',
+      sizeVariations: 5
+    });
+};
