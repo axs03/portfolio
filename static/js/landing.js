c6a39130f01ceeb3ea3c57e6421ce314dacedf1f
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-12-24 13:47:08 -0500

    added placeholder

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,8 +1,13 @@
-function loadContent(page) {
-    document.getElementById('contentFrame').src = page;
+function loadContent(url) {
+    var iframe = document.getElementById('contentFrame');
+    var placeholder = document.getElementById('placeholder');
+    iframe.src = url;
+    iframe.onload = function() {
+        placeholder.style.display = 'none';
+        iframe.style.display = 'block';
+    };
 }
 
 function scrollToContent() {
-    const contentBox = document.getElementById('content-box');
-    contentBox.scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('content-box').scrollIntoView({ behavior: 'smooth' });
 }
