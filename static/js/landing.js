f20afb2f9213e0dc9b0e23197d8387c7e869e496
Aman Sahu <144287512+as9219@users.noreply.github.com>
2024-12-24 15:23:15 -0500

    changed bg

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,7 +1,10 @@
+var currentPageUrl = ''; // glob
+
 function loadContent(url) {
     var iframe = document.getElementById('contentFrame');
     var placeholder = document.getElementById('placeholder');
     iframe.src = url;
+    currentPageUrl = url; // curr url
     iframe.onload = function() {
         placeholder.style.display = 'none';
         iframe.style.display = 'block';
@@ -12,14 +15,39 @@ function scrollToContent() {
     document.getElementById('content-box').scrollIntoView({ behavior: 'smooth' });
 }
 
+
 window.addEventListener('scroll', function() {
     var scrollPosition = window.scrollY;
-    var windowHeight = window.innerHeight;
-    var totalHeight = document.body.scrollHeight - windowHeight;
-    var scrollPercentage = scrollPosition / totalHeight;
+    var windowHeight   = window.innerHeight;
+    var totalHeight    = document.body.scrollHeight - windowHeight;
+    var scrollPercent  = scrollPosition / totalHeight;
 
-    var backgroundColor = `rgb(${Math.floor(17 + 238 * scrollPercentage)}, ${Math.floor(19 + 237 * scrollPercentage)}, ${Math.floor(22 + 234 * scrollPercentage)})`;
-    var particleColor = scrollPercentage > 0.5 ? '#ffffff' : '#5f82ed';
+    var backgroundColor, primaryParticleColor, secondaryParticleColor;
+
+    // normal blue to black
+    backgroundColor = `rgb(
+        ${Math.floor(17 - 17 * scrollPercent)},
+        ${Math.floor(19 - 22 * scrollPercent)},
+        ${Math.floor(22 - 22 * scrollPercent)}
+    )`;
+
+    primaryParticleColor = '#5f82ed';
+
+    if (currentPageUrl.includes('projects.html')) {
+        secondaryParticleColor = '#ffffff'; // white
+
+    } else if (currentPageUrl.includes('contact.html')) {
+        secondaryParticleColor = '#ffffff'; // white
+
+    } else if (currentPageUrl.includes('resume.html')) {
+        secondaryParticleColor = '#ffffff'; // white
+
+    } else {
+        return; // nothing present
+    }
+
+    // particle color
+    var particleColor = scrollPercent > 0.5 ? secondaryParticleColor : primaryParticleColor;
 
     document.querySelector('.background').style.backgroundColor = backgroundColor;
     particlesJS.particles.color.value = particleColor;
@@ -30,7 +58,7 @@ window.onload = function() {
     Particles.init({
         selector: '.background',
         connectParticles: true,
-        maxParticles: 200,
+        maxParticles: 150,
         color: '#5f82ed',
         sizeVariations: 5
     });
--- static/js/particle-bg.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    Particles.init({
-      selector: '.background',
-      connectParticles: true,
-      maxParticles: 200,
-      color: '#5f82ed',
-      sizeVariations: 5
-    });
-};
