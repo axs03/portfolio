ecbf47d63db8d50ee1340e065481a5c8b1b1a4c1
Aman Sahu <amansahu9000@gmail.com>
2025-02-25 23:53:03 -0500

    added pages and nav

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,14 +4,16 @@ import { HomeComponent } from '../templates/home/home.component';
 import { ExperienceComponent } from '../templates/experience/experience.component';
 import { ContactComponent } from '../templates/contact/contact.component';
 import { ProjectsComponent } from '../templates/projects/projects.component';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // imports: [RouterOutlet],
-  imports: [HomeComponent, ExperienceComponent, ContactComponent, ProjectsComponent],
+  imports: [HomeComponent, ExperienceComponent, ContactComponent, ProjectsComponent, NgbCollapseModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular';
+  title = 'Portfolio Home';
+  isMenuCollapsed = true;
 }
