78c9dd5276557b93ccd50bdcbd7deeded063e6d6
Aman Sahu <amansahu9000@gmail.com>
2025-02-26 13:15:16 -0500

    final design

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,19 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../templates/home/home.component';
 import { ExperienceComponent } from '../templates/experience/experience.component';
 import { ContactComponent } from '../templates/contact/contact.component';
 import { ProjectsComponent } from '../templates/projects/projects.component';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, ExperienceComponent, ContactComponent, ProjectsComponent, NgbCollapseModule, RouterLink],
+  imports: [HomeComponent, ExperienceComponent, ContactComponent, ProjectsComponent, NgbCollapseModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Portfolio Home';
   isMenuCollapsed = true;
+  activeSection: string = 'home';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngAfterViewInit(): void {
+    const sections = this.document.querySelectorAll('section');
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5 // 60% of the section in view
+    };
+    
+    const observer = new IntersectionObserver((entries) => {
+      let maxRatio = 0;
+      let activeId = this.activeSection;
+      
+      entries.forEach(entry => {
+        if (entry.intersectionRatio > maxRatio) {
+          maxRatio = entry.intersectionRatio;
+          activeId = entry.target.id;
+        }
+      });
+      
+      this.activeSection = activeId;
+    }, observerOptions);
+    
+    sections.forEach(section => observer.observe(section));
+  }
 }
