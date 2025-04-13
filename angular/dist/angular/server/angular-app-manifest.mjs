b3e0008001bcfddc5178124254b7157aa0562950
Aman Sahu <amansahu9000@gmail.com>
2025-04-12 23:36:51 -0400

    fixed titles and resume

--- angular/dist/angular/server/angular-app-manifest.mjs
+++ angular/dist/angular/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5719, hash: '14d394f93e57eb9feee30025a38c8979319aa60ecdc58013d2ba7b1f2466322e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1315, hash: '76c2a51e4328976a8b9e6f100590b70cf6fbafda91dbecd8724be06443dcb5b2', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 81002, hash: 'dd5d49b05bc05a303255fc1a137e25a9b42c4e030ec6a9ce8f94d050ad59675a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-PRFQ7FOV.css': {size: 232141, hash: 'o3IoxQREkgo', text: () => import('./assets-chunks/styles-PRFQ7FOV_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5718, hash: '8ebf935292b73b2ba9ce6eaed445c6add673057770ff2774819ace9bacd2c017', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1315, hash: '9df9cc5b282517f0fec6023884d2e7a70eb40b6794f001a33dc1ae8cb88bcc46', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 81677, hash: '9576a03bb72051c036d3d4d5f3a1fc2b5fa9b29bf73887d3de66789265801160', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-LZ6NN25T.css': {size: 232140, hash: 'mYPFbYIXIKY', text: () => import('./assets-chunks/styles-LZ6NN25T_css.mjs').then(m => m.default)}
   },
 };
