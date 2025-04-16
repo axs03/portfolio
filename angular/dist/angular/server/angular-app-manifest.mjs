f03d6af17a476df51a0458ffe62ec65bf3e71582
Aman Sahu <144287512+as9219@users.noreply.github.com>
2025-04-16 15:40:51 -0400

    fix: favicon=

--- angular/dist/angular/server/angular-app-manifest.mjs
+++ angular/dist/angular/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6948, hash: 'f8fa0dc934c4c17b0a12566849c3a110437ef3af243bf51a68af25d4282f7e4f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2553, hash: '0037d6d4dbf21e4ccac8de4afbc14913909ded5d6390b75668bbd67ffc8f8dc8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 82949, hash: 'bfacb61911d9c5f0126c6f8757d48ee3010db3f5e988a8170bd653401c23f317', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6958, hash: '53e2bd40a6de2b5ca8fde96a97ddf04f9cb17cac1496c6d15d0d44d5f0c0f159', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2563, hash: '3d2ff21f22fd61de84f516b8e3df05a6ef3ca03367cd33f1bfe1e85880a20e8c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 82959, hash: 'c2fed7b73a41b0964e167292821ad25055b1798b632688bb45db9760871d7d4c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-LZ6NN25T.css': {size: 232140, hash: 'mYPFbYIXIKY', text: () => import('./assets-chunks/styles-LZ6NN25T_css.mjs').then(m => m.default)}
   },
 };
