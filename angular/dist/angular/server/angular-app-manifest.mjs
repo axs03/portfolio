d9451c45892a6d036a8882afb65665741ec4fa4e
Aman Sahu <144287512+as9219@users.noreply.github.com>
2025-05-22 12:25:22 -0400

    updated resume/formatting

--- angular/dist/angular/server/angular-app-manifest.mjs
+++ angular/dist/angular/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6960, hash: '8d06e06092108ed6dab39365262c12c6396f82a3d14d964dd134378370cebbd2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 2563, hash: 'da7da7786333cdaf63fc2e157119a29b18be4406d4e262cbfcb0650597e37d9b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 83005, hash: '4e772fa030fe0d49dbc1f2b8502e8859b645183d82b6bb91972be2174f5e912b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-X4NG7N27.css': {size: 232142, hash: 'EZ9oihRQCNQ', text: () => import('./assets-chunks/styles-X4NG7N27_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7104, hash: 'a33617fa808b2046d8b0e4999a35b508b18ccfd2bb71299c00383c196affa004', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2563, hash: '677db83b565a80edffe8765f473ce4a4b018a7eecbcbcb9a759ddf411fbffad0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 83135, hash: 'e656961e170e04058000f89d2cab19c89692e98782836ac9d9c815eca976297d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-YIISL52A.css': {size: 231403, hash: 's/t/kiVP0Rs', text: () => import('./assets-chunks/styles-YIISL52A_css.mjs').then(m => m.default)}
   },
 };
